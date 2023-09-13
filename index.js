e622e640082c5a6b6a0213ed5386f6a2f590c557
Khushwinder127001) <khushwinder.php@gmail.com>
2023-09-13 23:33:46 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const http = require('http');
 const PORT = 3000;
+const cheerio = require("cheerio");
+const axios = require("axios");
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
 });
+
+axios.get("https://arstechnica.com/gadgets/").then(urlResponse =>{
+    const $ = cheerio.load(urlResponse.data);
+   
+ $('li.article').each((i, element)=>{
+    const link = $(element).find("a.overlay").attr("href");
+
+console.log(link);
+
+});
+});
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
